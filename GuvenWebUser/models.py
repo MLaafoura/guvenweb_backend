991c6df1a8d6daf777ede4b6eaecf6e66034f867
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-03-21 05:45:58 +0300

    user model

--- GuvenWebUser/models.py
+++ GuvenWebUser/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, fullName, phone_number,
-                    password=None, is_cyber_expert=False, is_client=False ):
+                    password=None, is_admin=False, is_cyber_expert=False, is_client=False ):
         if not email:
             raise ValueError('User must Have an email')
         
         email = self.normalize_email(email)
-        user = self.models(email=email, fullName=fullName, phone_number=phone_number,
+        user = self.model(email=email, fullName=fullName, phone_number=phone_number, is_admin=is_admin,
                            is_cyber_expert=is_cyber_expert, is_client=is_client)
         
         user.save()
@@ -21,9 +21,11 @@ class GuvenUser(AbstractBaseUser, PermissionsMixin):
     fullName = models.CharField(max_length=255)
     phone_number = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
     is_cyber_expert = models.BooleanField(default=False)
     is_client = models.BooleanField(default=False)
-
+    is_active = models.BooleanField(default=True)
+    
     objects = UserAccountManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['fullName', 'phone_number']
@@ -36,3 +38,5 @@ class GuvenUser(AbstractBaseUser, PermissionsMixin):
     
     def check_client(self):
         return self.is_client
+    
+
