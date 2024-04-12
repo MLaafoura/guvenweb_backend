9b4b4578640be46b1aaf9ee30aeedf692fac58ed
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-04-12 21:12:46 +0300

    [ADD] admin file

--- GuvenWebUser/admin.py
+++ GuvenWebUser/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
 from .models import GuvenUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(GuvenUser)
+
+class UserModelAdmin(BaseUserAdmin):
+    list_display = ('id', 'email', 'fullName', 'is_active', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('fullName',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'fullName', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email', 'id')
+    filter_horizontal = ()
+
+
+# Now register the new UserModelAdmin...
+admin.site.register(GuvenUser, UserModelAdmin)
